91a29eee0da1b31c13afa6e9e111044f9a3d1c1f
Lester Besabe <besabelester@gmail.com>
2017-12-26 14:30:47 +0800

    Units Dictionary Initial

---
+++ PhySol/src/Units.java
@@ -0,0 +1,17 @@
+class Units{
+
+
+
+/**Electricity**/
+	public static String[] voltage = {"volts", "volt", "V", "v"};
+	public static String[] current = {"amperes", "A", "amps"};
+	public static String[] resistance = {"ohms", "ohm", " Ω"};
+
+/**Kinematics**/
+	public static String[] speed = {"mps", "mips", "kmph", "kn", "kt", "knots", "fps"};
+	public static String[] acceleration = {"mps2"};
+	public static String[] velocity = {"mps", "mips", "kmph", "kn", "kt", "knots", "fps"};
+
+
+	
+}
