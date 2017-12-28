61806b01b3ecb7799c2eb6b6697fe5219eb99170
Lester Besabe <besabelester@gmail.com>
2017-12-28 12:33:46 +0800

    Formulas for Electricity

---
+++ PhySol/src/physics_area/Electricity.java
@@ -0,0 +1,146 @@
+package physics_area;
+
+public class Electricity {
+	
+
+//Voltage
+
+	/**
+	 * Formula for Voltage using Current and Resistance
+	 * @param g_current - Current Variable
+	 * @param g_resistance - Resistance Variable
+	 * @return double
+	 */
+	public static double elec_voltage_formula_cr(double g_current, double g_resistance){
+			
+			return g_current * g_resistance;
+	}
+	 
+	/**
+	 * Formula for Voltage using Power and Current
+	 * @param g_power
+	 * @param g_current
+	 * @return
+	 */
+	public static double elec_voltage_formula_pc(double g_power, double g_current){
+
+			return (g_power / g_current) + (g_power % g_current);
+	}
+	
+	/**
+	 * Formula for Voltage using Power and Resistance
+	 * @param g_power
+	 * @param g_resistance
+	 * @return
+	 */
+	public static double elec_voltage_formula_pr(double g_power, double g_resistance){
+
+			return Math.sqrt(g_power * g_resistance);
+	}
+
+
+//Resistance	
+	/**
+	 * Formula for Resistance using Voltage and Current
+	 * @param g_voltage - Voltage Variable
+	 * @param g_current - Current Variable
+	 * @return double 
+	 */
+	public static double elec_resistance_formula_vc(double g_voltage, double g_current){
+
+			return (g_voltage / g_current) + (g_voltage % g_current);
+	}
+	
+	/**
+	 * Formula for Resistance using Power and Current
+	 * @param g_power - Power Variable
+	 * @param g_current - Current Variable
+	 * @return double
+	 */
+	public static double elec_resistance_formula_pc(double g_power, double g_current){
+		
+			return (g_power / (Math.pow(g_current, 2))) + (g_power % (Math.pow(g_current, 2)));
+	}
+	
+	/**
+	 * Formula for Resistance using Voltage and Power
+	 * @param g_voltage - Voltage Variable
+	 * @param g_power - Power Variable
+	 * @return double
+	 */
+	public static double elec_resistance_formula_vp(double g_voltage, double g_power){
+			
+			return (Math.pow(g_voltage, 2)/ g_power) + (Math.pow(g_voltage,2) % g_power);
+			
+	}
+
+	
+//Current
+	/**
+	 * Formula for Current using Resistance and Voltage
+	 * @param g_resistance - Resistance Variable
+	 * @param g_voltage - Voltage Variable
+	 * @return double
+	 */
+	public static double elec_current_formula_rv(double g_resistance, double g_voltage){
+
+			return (g_voltage / g_resistance) + (g_voltage % g_resistance);
+	}
+
+	/**
+	 * Formula for Current using Power and Resistance
+	 * @param g_power - Power Variable
+	 * @param g_resistance - Resistance Variable
+	 * @return double
+	 */
+	public static double elec_current_formula_pr(double g_power, double g_resistance){
+
+			return Math.sqrt((g_power/g_resistance) + (g_power%g_resistance));
+	}
+	
+	/**
+	 * Formula for Current using Power and Voltage
+	 * @param g_power
+	 * @param g_voltage
+	 * @return double
+	 */
+	public static double elec_current_formula_pv(double g_power, double g_voltage){
+
+			return (g_power / g_voltage) + (g_power % g_voltage);
+	}
+	
+// Power
+	
+	/**
+	 * Formula for Power using Voltage and Current
+	 * @param g_voltage - Voltage Variable
+	 * @param g_current - Current Variable
+	 * @return double
+	 */
+	public static double elec_power_formula_vc(double g_voltage, double g_current){
+
+			return g_current * g_voltage;
+	}
+
+	/**
+	 * Formula for Power using Voltage and Resistance
+	 * @param g_voltage - Voltage Variable
+	 * @param g_resistance - Resistance Variable
+	 * @return double
+	 */
+	public static double elec_power_formula_vr(double g_voltage, double g_resistance){
+			double ans_whole = (Math.pow(g_voltage, 2))/ g_resistance;
+			double ans_rem = (Math.pow(g_voltage, 2)) % g_resistance;
+			return ans_whole + ans_rem;
+	}
+	/**
+	 * Formula for Power using Current and Resistance
+	 * @param g_current
+	 * @param g_resistance
+	 * @return double
+	 */
+	public static double elec_power_formula_cr(double g_current, double g_resistance){
+			return (Math.pow(g_current, 2)) * g_resistance;
+	}
+	
+}
