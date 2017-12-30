939c007ce8ebecbc97bcb572216dc17475e9effe
Lester Besabe <besabelester@gmail.com>
2017-12-30 20:47:11 +0800

    Electricity Formulas Complete

--- PhySol/src/physics_area/Electricity.java
+++ PhySol/src/physics_area/Electricity.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 public class Electricity {
 	
 	public static String[] derivation;
+	public static double final_ans;
 	
 //Voltage
 
@@ -15,13 +16,13 @@ public class Electricity {
 	 * @return double
 	 */
 	public static String[] elec_voltage_formula_cr(double g_current, double g_resistance, String g_current_unit, String g_resistance_unit){
-			double final_ans =  g_current * g_resistance;
+			final_ans =  g_current * g_resistance;
 			derivation = new String[5];
 			derivation[0] = "\tVoltage = Current * Resistance";
 			derivation[1] = "\tVoltage = " + g_current + " " + g_current_unit + " * "  + g_resistance + " " + g_resistance_unit;
-			derivation[2] = "\tVoltage = " + Double.toString(final_ans);
+			derivation[2] = "\tVoltage = " + final_ans;
 			derivation[3] = "\n";
-			derivation[4] = "Final Answer: " + Double.toString(final_ans) + " volts";
+			derivation[4] = "Final Answer: " + final_ans + " volts";
 			return derivation;
 	}
 	 
@@ -31,9 +32,17 @@ public class Electricity {
 	 * @param g_current
 	 * @return
 	 */
-	public static double elec_voltage_formula_pc(double g_power, double g_current){
-
-			return (g_power / g_current) + (g_power % g_current);
+	public static String[] elec_voltage_formula_pc(double g_power, double g_current, String g_power_unit, String g_current_unit){
+		
+			final_ans = (g_power / g_current);
+			
+			derivation = new String[5];
+			derivation[0] = "\tVoltage = Power / Current";
+			derivation[1] = "\tVoltage = " + g_power + " " + g_power_unit + " / " + g_current + " " + g_current_unit;
+			derivation[2] = "\tVoltage = " + final_ans;
+			derivation[3] = "\n";
+			derivation[4] = "Final Answer: " + final_ans + " volts";
+			return derivation;
 	}
 	
 	/**
@@ -42,9 +51,18 @@ public class Electricity {
 	 * @param g_resistance
 	 * @return+
 	 */
-	public static double elec_voltage_formula_pr(double g_power, double g_resistance){
+	public static String[] elec_voltage_formula_pr(double g_power, double g_resistance, String g_power_unit, String g_resistance_unit){
 
-			return Math.sqrt(g_power * g_resistance);
+			final_ans =  Math.sqrt(g_power * g_resistance);
+			
+			derivation = new String[6];
+			derivation[0] = "\tVoltage = \u221A(Power * Resistance)";
+			derivation[1] = "\tVoltage = sqrt(" + g_power + " " + g_power_unit + " * " + g_resistance + " " + g_resistance_unit + ")";
+			derivation[2] = "\tVoltage = sqrt(" + (g_power * g_resistance) + ")";
+			derivation[3] = "\tVoltage = " + final_ans;
+			derivation[4] = "\n";
+			derivation[5] = "Final Answer: " + final_ans + " volts";
+			return derivation;
 	}
 
 
@@ -55,9 +73,17 @@ public class Electricity {
 	 * @param g_current - Current Variable
 	 * @return double 
 	 */
-	public static double elec_resistance_formula_vc(double g_voltage, double g_current){
+	public static String[] elec_resistance_formula_vc(double g_voltage, double g_current, String g_voltage_unit, String g_current_unit){
 
-			return (g_voltage / g_current) + (g_voltage % g_current);
+			final_ans =  (g_voltage / g_current);
+			
+			derivation = new String[5];
+			derivation[0] = "\tResistance = Voltage / Current";
+			derivation[1] = "\tResistance = " + g_voltage + " " + g_voltage_unit + " / " + g_current + " " + g_current_unit;
+			derivation[2] = "\tResistance = " + final_ans;
+			derivation[3] = "\n";
+			derivation[4] = "Final Answer: " + final_ans + " ohms";
+			return derivation;
 	}
 	
 	/**
@@ -66,9 +92,18 @@ public class Electricity {
 	 * @param g_current - Current Variable
 	 * @return double
 	 */
-	public static double elec_resistance_formula_pc(double g_power, double g_current){
-		
-			return (g_power / (Math. pow(g_current, 2))) + (g_power % (Math.pow(g_current, 2)));
+	public static String[] elec_resistance_formula_pc(double g_power, double g_current, String g_power_unit, String g_current_unit){
+			
+			final_ans =  (g_power / (Math. pow(g_current, 2)));
+	
+			derivation = new String[6];
+			derivation[0] = "\tResistance = Power / Current\u00B2";
+			derivation[1] = "\tResistance = " + g_power +  " " +  g_power_unit  + " / (" + g_current + " " + g_current_unit + ")^2";
+			derivation[2] = "\tResistance = " + g_power + " " + g_power_unit + " / " + Math.pow(g_current, 2) + " " + g_current_unit;
+			derivation[3] = "\tResistance = " + final_ans;
+			derivation[4] = "\n";
+			derivation[5] = "Final Answer: " + final_ans + " ohms";
+			return derivation;
 	}
 	
 	/**
@@ -77,10 +112,18 @@ public class Electricity {
 	 * @param g_power - Power Variable
 	 * @return double
 	 */
-	public static double elec_resistance_formula_vp(double g_voltage, double g_power){
+	public static String[] elec_resistance_formula_vp(double g_voltage, double g_power, String g_voltage_unit, String g_power_unit){
 			
-			return (Math.pow(g_voltage, 2)/ g_power) + (Math.pow(g_voltage,2) % g_power);
+			final_ans =  (Math.pow(g_voltage, 2)/ g_power);
 			
+			derivation  = new String[6];
+			derivation[0] = "\tResistance = Voltage\u00B2 / Power";
+			derivation[1] = "\tResistance = (" + g_voltage + " " + g_voltage_unit + ")\u00B2" + " / " + g_power + " " + g_power_unit;
+			derivation[2] = "\tResistance = " +  Math.pow(g_voltage, 2) + " " + g_voltage_unit + " / " + g_power + " " + g_power_unit;
+			derivation[3] = "\tResistance = " + final_ans;
+			derivation[4] = "\n";
+			derivation[6] = "Final Answer: " + final_ans + " ohms";
+			return derivation;
 	}
 
 	
@@ -91,9 +134,17 @@ public class Electricity {
 	 * @param g_voltage - Voltage Variable
 	 * @return double
 	 */
-	public static double elec_current_formula_rv(double g_resistance, double g_voltage){
+	public static String[] elec_current_formula_vr(double g_voltage, double g_resistance, String g_voltage_unit, String g_resistance_unit){
 
-			return (g_voltage / g_resistance) + (g_voltage % g_resistance);
+			final_ans = (g_voltage / g_resistance);
+
+			derivation = new String[5];
+			derivation[0] = "\tCurrent = Voltage / Resistance";
+			derivation[1] = "\tCurrent = " + g_voltage + " " + g_voltage_unit + " / " + g_resistance + " " + g_resistance_unit;
+			derivation[2] = "\tCurrent = " + final_ans;
+			derivation[3] = "\n";
+			derivation[4] = "Final Answer: " + final_ans + " amperes";
+			return derivation;
 	}
 
 	/**
@@ -102,9 +153,18 @@ public class Electricity {
 	 * @param g_resistance - Resistance Variable
 	 * @return double
 	 */
-	public static double elec_current_formula_pr(double g_power, double g_resistance){
+	public static String[] elec_current_formula_pr(double g_power, double g_resistance, String g_power_unit, String g_resistance_unit){
 
-			return Math.sqrt((g_power/g_resistance) + (g_power%g_resistance));
+			final_ans =  Math.sqrt((g_power/g_resistance));
+
+			derivation = new String[6];
+			derivation[0] = "\tCurrent = \u221A(Power / Resistance)";
+			derivation[1] = "\tCurrent = \u221A (" + g_power + " " + g_power_unit + " / " + g_resistance + " " + g_resistance_unit;
+			derivation[2] = "\tCurrent = \u221A (" + (g_power/g_resistance) + ")";
+			derivation[3] = "\tCurrent = " + final_ans;
+			derivation[4] = "\n";
+			derivation[5] = "Final Answer: " + final_ans + " amperes";
+			return derivation;
 	}
 	
 	/**
@@ -113,9 +173,17 @@ public class Electricity {
 	 * @param g_voltage
 	 * @return double
 	 */
-	public static double elec_current_formula_pv(double g_power, double g_voltage){
+	public static String[] elec_current_formula_pv(double g_power, double g_voltage, String g_power_unit, String g_voltage_unit){
 
-			return (g_power / g_voltage) + (g_power % g_voltage);
+			final_ans =  (g_power / g_voltage);
+
+			derivation = new String[5];
+			derivation[0] = "\tCurrent = Power / Voltage";
+			derivation[1] = "\tCurrent = " + g_power + " " + g_power_unit + " / " + g_voltage + " " + g_voltage_unit;
+			derivation[2] = "\tCurrent = " + final_ans;
+			derivation[3] = "\n";
+			derivation[4] = "Final Answer: " + final_ans + " amperes";
+			return derivation;
 	}
 	
 // Power
@@ -126,9 +194,17 @@ public class Electricity {
 	 * @param g_current - Current Variable
 	 * @return double
 	 */
-	public static double elec_power_formula_vc(double g_voltage, double g_current){
+	public static String[] elec_power_formula_cv(double g_current, double g_voltage, String g_current_unit, String g_voltage_unit){
 
-			return g_current * g_voltage;
+			final_ans =  g_current * g_voltage;
+
+			derivation = new String[5];
+			derivation[0] = "\tPower = Current * Voltage";
+			derivation[1] = "\tPower = " + g_current + " " + g_current_unit + " * " + g_voltage + " " + g_voltage_unit;
+			derivation[2] = "\tPower = " + final_ans;
+			derivation[3] = "\n";
+			derivation[4] = "Final Answer: " + final_ans + " watts";
+			return derivation;
 	}
 
 	/**
@@ -137,10 +213,18 @@ public class Electricity {
 	 * @param g_resistance - Resistance Variable
 	 * @return double
 	 */
-	public static double elec_power_formula_vr(double g_voltage, double g_resistance){
-			double ans_whole = (Math.pow(g_voltage, 2))/ g_resistance;
-			double ans_rem = (Math.pow(g_voltage, 2)) % g_resistance;
-			return ans_whole + ans_rem;
+	public static String[] elec_power_formula_vr(double g_voltage, double g_resistance, String g_voltage_unit, String g_resistance_unit){
+			
+			final_ans = (Math.pow(g_voltage, 2))/ g_resistance;
+			
+			derivation = new String[6];
+			derivation[0] = "\tPower = Voltage\u00B2 / Resistance";
+			derivation[1] = "\tPower = (" + g_voltage + " " + g_voltage_unit + ")\u00B2 / " + g_resistance + " " + g_resistance_unit;
+			derivation[2] = "\tPower = " + Math.pow(g_voltage, 2) + " " + g_voltage_unit + " / " + g_resistance + " " + g_resistance_unit;
+			derivation[3] = "\tPower = " + final_ans;
+			derivation[4] = "\n";
+			derivation[5] = "Final Answer: " + final_ans + " watts";
+			return derivation;
 	}
 	/**
 	 * Formula for Power using Current and Resistance
@@ -148,9 +232,18 @@ public class Electricity {
 	 * @param g_resistance
 	 * @return double
 	 */
-	public static double elec_power_formula_cr(double g_current, double g_resistance){
+	public static String[] elec_power_formula_cr(double g_current, double g_resistance, String g_current_unit, String g_resistance_unit){
 		
-			return (Math.pow(g_current, 2)) * g_resistance;
+			final_ans =  (Math.pow(g_current, 2)) * g_resistance;
+
+			derivation = new String[6];
+			derivation[0] = "\tPower = Current\u00B2 * Resistance";
+			derivation[1] = "\tPower = (" + g_current + " " + g_current_unit + ")\u00B2 * " + g_resistance + " " + g_resistance_unit; 
+			derivation[2] = "\tPower = " + Math.pow(g_current, 2) + " " + g_current_unit + " * " + g_resistance + " " + g_resistance_unit;
+			derivation[3] = "\tPower = " + final_ans;
+			derivation[4] = "\n";
+			derivation[5] = "Final Answer: " + final_ans + " watts";
+			return derivation;
 	}
 	
 }
